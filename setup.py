2140db9788ca02e82df16514559746f93e90ea47
Zaar Hai <haizaar@haizaar.com>
2020-07-07 01:23:28 +1000

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("LICENSE") as f:
 
 setup(
     name="http-noah",
-    version="0.1.0",
+    version="0.1.1",
     description="Generic HTTP client for sync (requests) and async (aiohttp) operations",
     long_description=readme,
     author="Zaar Hai",
